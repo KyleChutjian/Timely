36d00b12817c15f7e2751a43ab81c7a0c64c9c58
AidanSchmid <aschmid@quinnipiac.com>
2022-03-24 12:44:51 -0400

    Updated Routes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 var express = require("express");
 const app = express();
 const users = require("./routes/users");
-
+const courses = require("./routes/courses");
+const lessondata = require("./routes/lessondata");
+const lessons = require("./routes/lessons");
 mongoose
   .connect(
     "mongodb+srv://fawaks15:Wizard51!@ecommerce.nfjwb.mongodb.net/Ecommerce?retryWrites=true&w=majority"
@@ -14,6 +16,9 @@ mongoose
 
 app.use(express.json());
 app.use("/users", users);
+app.use("/courses", courses);
+app.use("/lessondata", lessondata);
+app.use("/lessons", lessons);
 app.listen(5000, () => {
   console.log("Backend server is running!");
 });
--- routes/lessondata.js
+++ routes/lessondata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("This is users");
+  res.send("This is Lesson Data!");
 });
 
 module.exports = router;
