ac9dd2327390107c51ddaed05d56e9a32bed2448
KyleChutjian <kyle.chutjian@gmail.com>
2022-03-24 20:59:14 -0400

    Added an extra attribute to user

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    isProfessor: {type: Boolean, required: true}
 });
 
 module.exports = mongoose.model('UserModel', userSchema);
