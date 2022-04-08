178739dc0eed600f70e0b7853210a2f035e73b61
KyleChutjian <kyle.chutjian@gmail.com>
2022-04-08 19:03:04 -0400

    Merged course and entry schema files

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const Entry = require("../models/Entry.js");
 
+var entrySchema = new mongoose.Schema({
+  lessonId: {type: Number, required: true},
+  projectId: {type: Number, required: true},
+  homeworkId: {type: Number, required: true},
+  studyId: {type: Number, required: true},
+  courseCode: {type: Number, required: true},
+  userEmail: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users"},
+});
+
+
 const courseSchema = new mongoose.Schema({
   course_id: {type: Number, required: true},
   name: { type: String, required: true },
@@ -8,7 +18,7 @@ const courseSchema = new mongoose.Schema({
   numLessons: { type: Number, required: true },
   courseCode: { type: String, required: true },
   facultyEmail: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users"},
-  entry: {type: [mongoose.Schema], required: true, ref: "Entry"}
+  entry: {type: [entrySchema], required: true, ref: "Entry"}
 });
 
 module.exports = mongoose.model("Course", courseSchema);
